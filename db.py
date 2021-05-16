57be15e657e7d58bdce6523588fc6686617d4940
Daryna Matiiuk <dasha.matiyuk@gmail.com>
2021-05-16 22:35:26 +0300

    add backup&restore

--- db.py
+++ db.py
@@ -1,17 +1,34 @@
+import pymongo
 from pymongo import MongoClient
+
+import csv
 
 import os
 from dotenv import load_dotenv
 load_dotenv()
 
-cluster = MongoClient(os.getenv('DB_URL'))
-db = cluster["coursework-data-bases"]
-collection = db["products"]
+def get_data():
+    cluster = MongoClient(os.getenv('DB_URL'))
+    collection = cluster["coursework-data-bases"]
+    return collection["products"]
 
-post = {
-    "name": "milk",
-    "price": "20.00",
-    "currency": "uah"
- }
+def backup():
+    filepath = 'backup.csv'
+    cluster = MongoClient(os.getenv('DB_URL'))
+    data = cluster["coursework-data-bases"]["products"].find()
 
-collection.insert_one(post)
+    with open(filepath, 'w+') as file:
+        writer = csv.DictWriter(file, fieldnames=['name', 'price', 'currency'])
+        writer.writeheader()
+        for product in data:
+            del product['_id']
+            writer.writerow(product)
+
+def restore():
+    filepath = 'backup.csv'
+    cluster = pymongo.MongoClient(os.getenv('DB_URL'))
+    collection = cluster["coursework-data-bases"]["products"]
+    with open(filepath, 'r') as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            collection.insert_one(row)
