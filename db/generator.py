1855f2e92f5a63c269c2807b9df0bc37c6872a34
Daryna Matiiuk <dasha.matiyuk@gmail.com>
2021-05-19 21:16:12 +0300

    add connection with db

--- db/generator.py
+++ db/generator.py
@@ -1,49 +1,61 @@
 from faker import Faker
 import uuid
 import json
+from repository import ProductsRepository
 
 faker = Faker()
+PR = ProductsRepository()
 
 PRODUCTS_QUANTITY = 10
-file = 'products.json'
+filepath = 'db/data.json'
 products = {}
 
 def generate_cheese():
-    products['cheese'] = []
+    type = 'cheese'
+    products[type] = []
     for i in range(PRODUCTS_QUANTITY):
-        products['cheese'].append({
-            'id': f'{uuid.uuid4()}',
-            'type': 'cheese',
+        products[type].append({
+            '_id': f'{uuid.uuid4()}',
+            'type': type,
             'name': faker.words(1)[0],
             'price': f'{faker.random_int(30, 1000)},{faker.random_int(0, 99)}',
             'currency': 'uah'
         })
     write_to_json()
+    write_data_to_db(type)
 
 def generate_milk():
-    products['milk'] = []
+    type = 'milk'
+    products[type] = []
     for i in range(PRODUCTS_QUANTITY):
-        products['milk'].append({
-            'id': f'{uuid.uuid4()}',
-            'type': 'milk',
+        products[type].append({
+            '_id': f'{uuid.uuid4()}',
+            'type': type,
             'name': faker.words(1)[0],
             'price': f'{faker.random_int(10, 100)},{faker.random_int(0, 99)}',
             'currency': 'uah'
         })
     write_to_json()
+    write_data_to_db(type)
 
 def generate_bread():
-    products['bread'] = []
+    type = 'bread'
+    products[type] = []
     for i in range(PRODUCTS_QUANTITY):
-        products['bread'].append({
-            'id': f'{uuid.uuid4()}',
-            'type': 'bread',
+        products[type].append({
+            '_id': f'{uuid.uuid4()}',
+            'type': type,
             'name': faker.words(1)[0],
             'price': f'{faker.random_int(10, 500)},{faker.random_int(0, 99)}',
             'currency': 'uah'
         })
     write_to_json()
+    write_data_to_db(type)
 
 def write_to_json():
-    with open('data.json', 'w+') as outfile:
+    with open(filepath, 'w+') as outfile:
         json.dump(products, outfile, ensure_ascii=False, indent=4)
+
+def write_data_to_db(type):
+    for p in products[type]:
+        PR.insert_one(p)
